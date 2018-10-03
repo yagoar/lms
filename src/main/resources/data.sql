f11ce6d7df6b47e95a6b67deada7c5961b9a0a15
ygo <yaiza.gonzalo-alt@iteratec.de>
2018-10-03 16:33:51 +0200

    Test

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,84 +1,94 @@
-INSERT INTO laufbahnstufe VALUES (1, 'Software Engineer');
-INSERT INTO laufbahnstufe_pflicht_kompetenzen VALUES (1, 'SE', 'GL');
-INSERT INTO laufbahnstufe_pflicht_kompetenzen VALUES (1, 'SKF', 'GL');
-INSERT INTO laufbahnstufe_pflicht_kompetenzen VALUES (1, 'UDH', 'GL');
-INSERT INTO laufbahnstufe_optional_kompetenzen VALUES (1, 'AM', 'GL');
-INSERT INTO laufbahnstufe_optional_kompetenzen VALUES (1, 'SA', 'GL');
-INSERT INTO laufbahnstufe_optional_kompetenzen VALUES (1, 'QM', 'GL');
-INSERT INTO laufbahnstufe_optional_kompetenzen VALUES (1, 'ITM', 'GL');
-INSERT INTO laufbahnstufe_optional_kompetenzen VALUES (1, 'PM', 'GL');
-
-INSERT INTO laufbahnstufe VALUES (2, 'Senior Software Engineer');
-INSERT INTO laufbahnstufe_pflicht_kompetenzen VALUES (2, 'SE', 'GL');
-INSERT INTO laufbahnstufe_pflicht_kompetenzen VALUES (2, 'AM', 'GL');
-INSERT INTO laufbahnstufe_pflicht_kompetenzen VALUES (2, 'SA', 'GL');
-INSERT INTO laufbahnstufe_pflicht_kompetenzen VALUES (2, 'QM', 'GL');
-INSERT INTO laufbahnstufe_pflicht_kompetenzen VALUES (2, 'PM', 'GL');
-INSERT INTO laufbahnstufe_pflicht_kompetenzen VALUES (2, 'SKF', 'GL');
-INSERT INTO laufbahnstufe_pflicht_kompetenzen VALUES (2, 'UDH', 'GL');
-
-INSERT INTO laufbahnstufe_optional_kompetenzen VALUES (2, 'SE', 'FU');
-INSERT INTO laufbahnstufe_optional_kompetenzen VALUES (2, 'AM', 'FU');
-INSERT INTO laufbahnstufe_optional_kompetenzen VALUES (2, 'SA', 'FU');
-INSERT INTO laufbahnstufe_optional_kompetenzen VALUES (2, 'QM', 'FU');
-INSERT INTO laufbahnstufe_optional_kompetenzen VALUES (2, 'ITM', 'FU');
-INSERT INTO laufbahnstufe_optional_kompetenzen VALUES (2, 'PM', 'FU');
-INSERT INTO laufbahnstufe_optional_kompetenzen VALUES (2, 'SKF', 'FU');
-INSERT INTO laufbahnstufe_optional_kompetenzen VALUES (2, 'BPK', 'FU');
-
-INSERT INTO laufbahnstufe VALUES (3, 'Software Architect / Lead Software Engineer');
-INSERT INTO laufbahnstufe_pflicht_kompetenzen VALUES (3, 'SE', 'FU');
-INSERT INTO laufbahnstufe_pflicht_kompetenzen VALUES (3, 'AM', 'GL');
-INSERT INTO laufbahnstufe_pflicht_kompetenzen VALUES (3, 'SA', 'FU');
-INSERT INTO laufbahnstufe_pflicht_kompetenzen VALUES (3, 'QM', 'GL');
-INSERT INTO laufbahnstufe_pflicht_kompetenzen VALUES (3, 'PM', 'GL');
-INSERT INTO laufbahnstufe_pflicht_kompetenzen VALUES (3, 'SKF', 'FU');
-INSERT INTO laufbahnstufe_pflicht_kompetenzen VALUES (3, 'UDH', 'FU');
-INSERT INTO laufbahnstufe_pflicht_kompetenzen VALUES (3, 'BPK', 'GL');
-INSERT INTO laufbahnstufe_pflicht_kompetenzen VALUES (3, 'AKQ', 'GL');
-
-
-INSERT INTO laufbahnstufe_optional_kompetenzen VALUES (3, 'SE', 'EX');
-INSERT INTO laufbahnstufe_optional_kompetenzen VALUES (3, 'SA', 'EX');
-INSERT INTO laufbahnstufe_optional_kompetenzen VALUES (3, 'QM', 'FU');
-INSERT INTO laufbahnstufe_optional_kompetenzen VALUES (3, 'ITM', 'GL');
-INSERT INTO laufbahnstufe_optional_kompetenzen VALUES (3, 'PM', 'FU');
-
-
-INSERT INTO laufbahnstufe VALUES (4, 'Senior Solution Architect / Senior Software Architect / Senior Lead Software Engineer');
-INSERT INTO laufbahnstufe_pflicht_kompetenzen VALUES (4, 'SE', 'EX');
-INSERT INTO laufbahnstufe_pflicht_kompetenzen VALUES (4, 'AM', 'GL');
-INSERT INTO laufbahnstufe_pflicht_kompetenzen VALUES (4, 'SA', 'EX');
-INSERT INTO laufbahnstufe_pflicht_kompetenzen VALUES (4, 'QM', 'FU');
-INSERT INTO laufbahnstufe_pflicht_kompetenzen VALUES (4, 'PM', 'GL');
-INSERT INTO laufbahnstufe_pflicht_kompetenzen VALUES (4, 'SKF', 'FU');
-INSERT INTO laufbahnstufe_pflicht_kompetenzen VALUES (4, 'UDH', 'FU');
-INSERT INTO laufbahnstufe_pflicht_kompetenzen VALUES (4, 'BPK', 'FU');
-INSERT INTO laufbahnstufe_pflicht_kompetenzen VALUES (4, 'AKQ', 'FU');
-
-
-INSERT INTO laufbahnstufe_optional_kompetenzen VALUES (4, 'AM', 'FU');
-INSERT INTO laufbahnstufe_optional_kompetenzen VALUES (4, 'ITM', 'GL');
-INSERT INTO laufbahnstufe_optional_kompetenzen VALUES (4, 'PM', 'FU');
-INSERT INTO laufbahnstufe_optional_kompetenzen VALUES (4, 'SKF', 'EX');
-INSERT INTO laufbahnstufe_optional_kompetenzen VALUES (4, 'UDH', 'EX');
-INSERT INTO laufbahnstufe_optional_kompetenzen VALUES (4, 'BPK', 'EX');
-
-INSERT INTO laufbahnstufe VALUES (5, 'Principal Solution Architect / Principal Software Architect');
-INSERT INTO laufbahnstufe_pflicht_kompetenzen VALUES (5, 'SE', 'EX');
-INSERT INTO laufbahnstufe_pflicht_kompetenzen VALUES (5, 'AM', 'FU');
-INSERT INTO laufbahnstufe_pflicht_kompetenzen VALUES (5, 'SA', 'FR');
-INSERT INTO laufbahnstufe_pflicht_kompetenzen VALUES (5, 'QM', 'FU');
-INSERT INTO laufbahnstufe_pflicht_kompetenzen VALUES (5, 'PM', 'FU');
-INSERT INTO laufbahnstufe_pflicht_kompetenzen VALUES (5, 'SKF', 'EX');
-INSERT INTO laufbahnstufe_pflicht_kompetenzen VALUES (5, 'UDH', 'EX');
-INSERT INTO laufbahnstufe_pflicht_kompetenzen VALUES (5, 'BPK', 'FU');
-INSERT INTO laufbahnstufe_pflicht_kompetenzen VALUES (5, 'AKQ', 'FU');
-
-
-INSERT INTO laufbahnstufe_optional_kompetenzen VALUES (5, 'SE', 'FR');
-INSERT INTO laufbahnstufe_optional_kompetenzen VALUES (5, 'QM', 'EX');
-INSERT INTO laufbahnstufe_optional_kompetenzen VALUES (5, 'ITM', 'GL');
-INSERT INTO laufbahnstufe_optional_kompetenzen VALUES (5, 'UDH', 'FR');
-INSERT INTO laufbahnstufe_optional_kompetenzen VALUES (5, 'BPK', 'EX');
-INSERT INTO laufbahnstufe_optional_kompetenzen VALUES (5, 'AKQ', 'EX');
+-- INSERT INTO laufbahnstufe VALUES (1, 0, 'Junior', 'BASIS');
+-- INSERT INTO laufbahnstufe_pflicht_kompetenzen VALUES (1, 'SE', 'GL');
+-- INSERT INTO laufbahnstufe_pflicht_kompetenzen VALUES (1, 'SKF', 'GL');
+-- INSERT INTO laufbahnstufe_pflicht_kompetenzen VALUES (1, 'UDH', 'GL');
+-- INSERT INTO laufbahnstufe_optional_kompetenzen VALUES (1, 'AM', 'GL');
+-- INSERT INTO laufbahnstufe_optional_kompetenzen VALUES (1, 'SA', 'GL');
+-- INSERT INTO laufbahnstufe_optional_kompetenzen VALUES (1, 'QM', 'GL');
+-- INSERT INTO laufbahnstufe_optional_kompetenzen VALUES (1, 'ITM', 'GL');
+-- INSERT INTO laufbahnstufe_optional_kompetenzen VALUES (1, 'PM', 'GL');
+--
+-- INSERT INTO laufbahnstufe VALUES (2, 1, 'Software Engineer', 'BASIS');
+-- INSERT INTO laufbahnstufe_pflicht_kompetenzen VALUES (2, 'SE', 'GL');
+-- INSERT INTO laufbahnstufe_pflicht_kompetenzen VALUES (2, 'SKF', 'GL');
+-- INSERT INTO laufbahnstufe_pflicht_kompetenzen VALUES (2, 'UDH', 'GL');
+-- INSERT INTO laufbahnstufe_optional_kompetenzen VALUES (2, 'AM', 'GL');
+-- INSERT INTO laufbahnstufe_optional_kompetenzen VALUES (2, 'SA', 'GL');
+-- INSERT INTO laufbahnstufe_optional_kompetenzen VALUES (2, 'QM', 'GL');
+-- INSERT INTO laufbahnstufe_optional_kompetenzen VALUES (2, 'ITM', 'GL');
+-- INSERT INTO laufbahnstufe_optional_kompetenzen VALUES (2, 'PM', 'GL');
+--
+-- INSERT INTO laufbahnstufe VALUES (3, 2, 'Senior Software Engineer', 'BASIS');
+-- INSERT INTO laufbahnstufe_pflicht_kompetenzen VALUES (3, 'SE', 'GL');
+-- INSERT INTO laufbahnstufe_pflicht_kompetenzen VALUES (3, 'AM', 'GL');
+-- INSERT INTO laufbahnstufe_pflicht_kompetenzen VALUES (3, 'SA', 'GL');
+-- INSERT INTO laufbahnstufe_pflicht_kompetenzen VALUES (3, 'QM', 'GL');
+-- INSERT INTO laufbahnstufe_pflicht_kompetenzen VALUES (3, 'PM', 'GL');
+-- INSERT INTO laufbahnstufe_pflicht_kompetenzen VALUES (3, 'SKF', 'GL');
+-- INSERT INTO laufbahnstufe_pflicht_kompetenzen VALUES (3, 'UDH', 'GL');
+--
+-- INSERT INTO laufbahnstufe_optional_kompetenzen VALUES (3, 'SE', 'FU');
+-- INSERT INTO laufbahnstufe_optional_kompetenzen VALUES (3, 'AM', 'FU');
+-- INSERT INTO laufbahnstufe_optional_kompetenzen VALUES (3, 'SA', 'FU');
+-- INSERT INTO laufbahnstufe_optional_kompetenzen VALUES (3, 'QM', 'FU');
+-- INSERT INTO laufbahnstufe_optional_kompetenzen VALUES (3, 'ITM', 'FU');
+-- INSERT INTO laufbahnstufe_optional_kompetenzen VALUES (3, 'PM', 'FU');
+-- INSERT INTO laufbahnstufe_optional_kompetenzen VALUES (3, 'SKF', 'FU');
+-- INSERT INTO laufbahnstufe_optional_kompetenzen VALUES (3, 'BPK', 'FU');
+--
+-- INSERT INTO laufbahnstufe VALUES (4, 3, 'Software Architect / Lead Software Engineer', 'TECHNOLOGIE_ARCHITEKTUR');
+-- INSERT INTO laufbahnstufe_pflicht_kompetenzen VALUES (4, 'SE', 'FU');
+-- INSERT INTO laufbahnstufe_pflicht_kompetenzen VALUES (4, 'AM', 'GL');
+-- INSERT INTO laufbahnstufe_pflicht_kompetenzen VALUES (4, 'SA', 'FU');
+-- INSERT INTO laufbahnstufe_pflicht_kompetenzen VALUES (4, 'QM', 'GL');
+-- INSERT INTO laufbahnstufe_pflicht_kompetenzen VALUES (4, 'PM', 'GL');
+-- INSERT INTO laufbahnstufe_pflicht_kompetenzen VALUES (4, 'SKF', 'FU');
+-- INSERT INTO laufbahnstufe_pflicht_kompetenzen VALUES (4, 'UDH', 'FU');
+-- INSERT INTO laufbahnstufe_pflicht_kompetenzen VALUES (4, 'BPK', 'GL');
+-- INSERT INTO laufbahnstufe_pflicht_kompetenzen VALUES (4, 'AKQ', 'GL');
+--
+--
+-- INSERT INTO laufbahnstufe_optional_kompetenzen VALUES (4, 'SE', 'EX');
+-- INSERT INTO laufbahnstufe_optional_kompetenzen VALUES (4, 'SA', 'EX');
+-- INSERT INTO laufbahnstufe_optional_kompetenzen VALUES (4, 'QM', 'FU');
+-- INSERT INTO laufbahnstufe_optional_kompetenzen VALUES (4, 'ITM', 'GL');
+-- INSERT INTO laufbahnstufe_optional_kompetenzen VALUES (4, 'PM', 'FU');
+--
+--
+-- INSERT INTO laufbahnstufe VALUES (5, 4, 'Senior Solution Architect / Senior Software Architect / Senior Lead Software Engineer', 'TECHNOLOGIE_ARCHITEKTUR');
+-- INSERT INTO laufbahnstufe_pflicht_kompetenzen VALUES (5, 'SE', 'EX');
+-- INSERT INTO laufbahnstufe_pflicht_kompetenzen VALUES (5, 'AM', 'GL');
+-- INSERT INTO laufbahnstufe_pflicht_kompetenzen VALUES (5, 'SA', 'EX');
+-- INSERT INTO laufbahnstufe_pflicht_kompetenzen VALUES (5, 'QM', 'FU');
+-- INSERT INTO laufbahnstufe_pflicht_kompetenzen VALUES (5, 'PM', 'GL');
+-- INSERT INTO laufbahnstufe_pflicht_kompetenzen VALUES (5, 'SKF', 'FU');
+-- INSERT INTO laufbahnstufe_pflicht_kompetenzen VALUES (5, 'UDH', 'FU');
+-- INSERT INTO laufbahnstufe_pflicht_kompetenzen VALUES (5, 'BPK', 'FU');
+-- INSERT INTO laufbahnstufe_pflicht_kompetenzen VALUES (5, 'AKQ', 'FU');
+--
+--
+-- INSERT INTO laufbahnstufe_optional_kompetenzen VALUES (5, 'AM', 'FU');
+-- INSERT INTO laufbahnstufe_optional_kompetenzen VALUES (5, 'ITM', 'GL');
+-- INSERT INTO laufbahnstufe_optional_kompetenzen VALUES (5, 'PM', 'FU');
+-- INSERT INTO laufbahnstufe_optional_kompetenzen VALUES (5, 'SKF', 'EX');
+-- INSERT INTO laufbahnstufe_optional_kompetenzen VALUES (5, 'UDH', 'EX');
+-- INSERT INTO laufbahnstufe_optional_kompetenzen VALUES (5, 'BPK', 'EX');
+--
+-- INSERT INTO laufbahnstufe VALUES (6, 5, 'Principal Solution Architect / Principal Software Architect', 'TECHNOLOGIE_ARCHITEKTUR');
+-- INSERT INTO laufbahnstufe_pflicht_kompetenzen VALUES (6, 'SE', 'EX');
+-- INSERT INTO laufbahnstufe_pflicht_kompetenzen VALUES (6, 'AM', 'FU');
+-- INSERT INTO laufbahnstufe_pflicht_kompetenzen VALUES (6, 'SA', 'FR');
+-- INSERT INTO laufbahnstufe_pflicht_kompetenzen VALUES (6, 'QM', 'FU');
+-- INSERT INTO laufbahnstufe_pflicht_kompetenzen VALUES (6, 'PM', 'FU');
+-- INSERT INTO laufbahnstufe_pflicht_kompetenzen VALUES (6, 'SKF', 'EX');
+-- INSERT INTO laufbahnstufe_pflicht_kompetenzen VALUES (6, 'UDH', 'EX');
+-- INSERT INTO laufbahnstufe_pflicht_kompetenzen VALUES (6, 'BPK', 'FU');
+-- INSERT INTO laufbahnstufe_pflicht_kompetenzen VALUES (6, 'AKQ', 'FU');
+--
+--
+-- INSERT INTO laufbahnstufe_optional_kompetenzen VALUES (6, 'SE', 'FR');
+-- INSERT INTO laufbahnstufe_optional_kompetenzen VALUES (6, 'QM', 'EX');
+-- INSERT INTO laufbahnstufe_optional_kompetenzen VALUES (6, 'ITM', 'GL');
+-- INSERT INTO laufbahnstufe_optional_kompetenzen VALUES (6, 'UDH', 'FR');
+-- INSERT INTO laufbahnstufe_optional_kompetenzen VALUES (6, 'BPK', 'EX');
+-- INSERT INTO laufbahnstufe_optional_kompetenzen VALUES (6, 'AKQ', 'EX');
